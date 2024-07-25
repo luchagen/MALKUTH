bfd23a9fbe1abed7a6fe7cd22d37f4f134453b3e
luchagen <ts417.luc.hagen@gmail.com>
2024-07-25 17:19:46 +0400

    parameters file for setup

---
+++ parameters.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 16:19:56 2023
+
+@author: suric
+"""
+
+discord_api_key="Put your discord api key here"
+youtube_api_key="api key for youtube interactions"
+openai_api_key="api key for openai interactions"
+sync_channels = {}  #default channels to sync to one another. deprecated.
+goodresultemoji=1 #feedback mechanism emoji
+midresultemoji=2 #feedback mechanism emoji
+badresultemoji=3 #feedback mechanism emoji
+wrongcommandmessage=' message to send when user sends a message he should not'
